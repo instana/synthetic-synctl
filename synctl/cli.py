720cae3893b666d3855bb2013192bcc42a60c0f8
swethalohith2 <swetha.lohith@ibm.com>
2024-01-22 14:21:49 +0530

    add update cred

--- synctl/cli.py
+++ synctl/cli.py
@@ -1091,6 +1091,58 @@ class SyntheticCredential(Base):
         total_time = round((end_time-start_time)*1000, 3)
         print(f"total deleted: {total_number}, time used: {total_time}ms")
 
+    def update_a_credential(self, cred):
+        self.check_host_and_token(self.auth["host"], self.auth["token"])
+        if cred is None:
+            print("credential should not be empty")
+            return
+
+        credential = self.retrieve_credentials()
+
+        host = self.auth["host"]
+        token = self.auth["token"]
+
+        # delete cred
+        delete_url = f"{host}/api/synthetics/settings/credentials/{cred}"
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"apiToken {token}"
+        }
+        if cred in credential:
+            delete_res = requests.delete(delete_url,
+                                         headers=headers,
+                                         timeout=60,
+                                         verify=self.insecure)
+
+            if _status_is_204(delete_res.status_code):
+                pass
+            else:
+                print(
+                    f"Fail to update {cred}, status code {delete_res.status_code}")
+        else:
+            self.exit_synctl(ERROR_CODE, f"no credential {cred}")
+
+        # create cred
+        create_url = f"{host}/api/synthetics/settings/credentials/"
+
+        credential = self.retrieve_credentials()
+        cred_payload = self.payload
+
+        if cred not in credential:
+            create_cred_res = requests.post(create_url,
+                                            headers=headers,
+                                            data=cred_payload,
+                                            timeout=60,
+                                            verify=self.insecure)
+            if _status_is_201(create_cred_res.status_code):
+                print(f"Credential \"{cred}\" updated")
+            else:
+                print('Update credential failed, status code:',
+                      create_cred_res.status_code)
+        else:
+            print("Credential already exists")
+
+
     def print_credentials(self, credentials):
         sorted_cred = sorted(credentials, key=str.lower)
         for cred in sorted_cred:
@@ -3693,7 +3745,7 @@ class ParseParameter:
 
     def update_command_options(self):
         self.parser_update.add_argument(
-            'syn_type', type=str, choices=["test", "alert"], help="Synthetic type/ smart alert")
+            'syn_type', type=str, choices=["test", "alert", "cred"], help="Synthetic type/ smart alert/ credential")
         self.parser_update.add_argument(
             'id', type=str, help="Synthetic test id")
 
@@ -3761,7 +3813,9 @@ class ParseParameter:
             '--enable', action='store_true', help='enable smart alert')
         update_exclusive_group.add_argument(
             '--disable', action='store_true', help='disable smart alert')
-
+        #update cred
+        update_group.add_argument(
+            '--value', type=str, metavar="<value>", help='set credential value')
 
         self.parser_update.add_argument(
             '--use-env', '-e', type=str, metavar="<name>", default=None, help='use a config hostname')
@@ -4331,6 +4385,14 @@ def main():
                     update_alert.update_tag_filter_expression(tag_filter_expression)
                 updated_alert_config = update_alert.get_updated_alert_config()
                 update_alert.update_a_smart_alert(get_args.id, updated_alert_config)
+        if get_args.syn_type == SYN_CRED:
+            cred_payload = CredentialConfiguration()
+            if get_args.value is not None:
+                cred_payload.set_credential_name(get_args.id)
+                cred_payload.set_credential_value(get_args.value)
+
+            cred_instance.set_cred_payload(payload=cred_payload.get_json())
+            cred_instance.update_a_credential(get_args.id)
     elif COMMAND_DELETE == get_args.sub_command:
         if get_args.delete_type == SYN_TEST:
             if get_args.id is not None and len(get_args.id) > 0:
