57cda3a9704b05dee48e4d31fb1972a5ec3c3109
swethalohith2 <swetha.lohith@ibm.com>
2024-04-30 15:27:49 +0530

    update readme

--- synctl/cli.py
+++ synctl/cli.py
@@ -5043,7 +5043,7 @@ def main():
 
                 if get_args.type == 5:
                     # SSL Certificate test run every 24 hours
-                    get_args.frequency = 24
+                    # get_args.frequency = 24
                     if get_args.hostname is not None:
                         payload.set_host(get_args.hostname)
                     if get_args.port is not None:
